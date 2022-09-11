f405d5c01e95d0258e6787ca886fe44d15652cf6
cjf8140 <cjf8140@gmail.com>
2022-09-11 13:42:19 +0900

    Update rps_game.py

--- rps_game.py
+++ rps_game.py
@@ -59,11 +59,10 @@ while cap.isOpened():
 
             angle = np.arccos(np.einsum('nt,nt->n',
                 v[[0,1,2,4,5,6,8,9,10,12,13,14,16,17,18],:],
-                v[[1,2,3,5,6,7,9,10,11,13,14,15,17,18,19],:])) # [15,]
+                v[[1,2,3,5,6,7,9,10,11,13,14,15,17,18,19],:]))
 
-            angle = np.degrees(angle) # Convert radian to degree
+            angle = np.degrees(angle)
 
-            # Inference gesture
             data = np.array([angle], dtype=np.float32)
             ret, results, neighbours, dist = knn.findNearest(data, 3)
             idx = int(results[0][0])
